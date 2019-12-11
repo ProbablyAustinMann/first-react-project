62692147dd00c620905d9e96180d3abe3eef414f
Austin Taylor Mann <Fancysause@gmail.com>
2019-12-11 14:45:06 -0500

    added button and method to change person information, changed app class to method to use hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Austin', age: 26 },
+      { name: 'Rodney', age: 7 },
+      { name: 'Nolan', age: 23 }
+    ],
+    //otherState: 'some other value'
+  });
+
+const [ otherState, setOtherState ] = useState('some other value');
+
+console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    //Function to change displayed data with a button click
+    setPersonsState({
+      persons: [
+        { name: 'Aus', age: 26 },
+        { name: 'Rod', age: 7 },
+        { name: 'Nolan', age: 25 }
+      ]
+    });
+  };
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>It is working!</p>
-        <Person name='Austin' age='26' />
-        <Person name='Rodney' age='7' >My Hobbies: Sleeping</Person>
-        <Person name='Nolan' age='23' />
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Sleeping</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
       </div>
     );
 //      return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
-  }
-}
+};
 
-export default App;
+export default app;
