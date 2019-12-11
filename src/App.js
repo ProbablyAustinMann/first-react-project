0350ea1009f248f8db2511aca87203e42d25fff9
Austin Taylor Mann <Fancysause@gmail.com>
2019-12-11 00:05:43 -0500

    added 'person' component and dynamic person atributes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-//    return (
-//      <div className="App">
-//        <h1>Hi, I am a React App</h1>
-//      </div>
-//    );
-      return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <p>It is working!</p>
+        <Person name='Austin' age='26' />
+        <Person name='Rodney' age='7' >My Hobbies: Sleeping</Person>
+        <Person name='Nolan' age='23' />
+      </div>
+    );
+//      return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
